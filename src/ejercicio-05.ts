b499f023f865a8c6e61b094dad6f89eef263b75c
devrodrigolec <devrodrigolec@gmail.com>
2024-12-01 21:25:46 +0100

    ejercicio 5 finalizado

--- src/ejercicio-05.ts
+++ src/ejercicio-05.ts
@@ -1,1 +1,45 @@
 console.log("************** DELIVERABLE 05 *********************");
+
+class SlotMachine {
+  #coins: number = 0;
+
+  createRandomBoolean() {
+    return Math.random() > 0.5
+  }
+
+  userWon() {
+    console.log(`Congratulations!!!. You won ${this.#coins} coins!!!`);
+    this.#coins = 0;
+  }
+
+  userLoose() {
+    console.log("Good luck next time!!");
+  }
+
+  play() {
+    this.#coins++;
+    const slots = [
+      this.createRandomBoolean(),
+      this.createRandomBoolean(),
+      this.createRandomBoolean(),
+    ];
+
+    slots.every((e: boolean) => e) ? this.userWon() : this.userLoose();
+  }
+}
+
+const machine1 = new SlotMachine();
+
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
